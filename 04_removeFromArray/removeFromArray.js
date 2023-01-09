d84677f362443757ff27951ee7b2d94c390c5d50
ryanleeyoong <shapiferoaf@gmail.com>
2023-01-10 04:21:21 +0800

    uncompleted removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, arg) {
+    let index;
+    let newArray;
 
+    index = array.indexOf(arg);
+    newArray = array.splice(index,1);
+
+    return newArray;
 };
 
 // Do not edit below this line
