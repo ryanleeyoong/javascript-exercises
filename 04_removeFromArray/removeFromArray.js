c9b395d365ec2c3560724ce5eb0902ce9574d6e7
ryanleeyoong <shapiferoaf@gmail.com>
2023-01-10 21:04:30 +0800

    completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,24 @@
-const removeFromArray = function(array, arg) {
-    let index;
-    let newArray;
+const removeFromArray = function(...args) {
+    const array = args[0];
+    const newArray = [];
 
-    index = array.indexOf(arg);
-    newArray = array.splice(index,1);
+    array.forEach( item => {
+        if (!args.includes(item)){
+            newArray.push(item);
+        }
+    });
 
     return newArray;
 };
+
+//shorter better answer
+// var removeFromArray = function(...args) {
+//   const array = args[0]
+//   return array.filter(val => !args.includes(val))
+// }
+
+//notes:
+//realise I'm weak in Array methods, will need to work on that.
 
 // Do not edit below this line
 module.exports = removeFromArray;
