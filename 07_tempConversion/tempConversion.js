cb21c43d1ec072a51213d975ba574d4b7609ac1f
ryanleeyoong <shapiferoaf@gmail.com>
2023-01-11 23:03:03 +0800

    completed tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  let convertedC = 5/9 * (tempF - 32);
+  return Math.round(convertedC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempC) {
+  let convertedF = 9/5 * (tempC) + 32;
+  return Math.round(convertedF * 10) / 10;
 };
 
 // Do not edit below this line
