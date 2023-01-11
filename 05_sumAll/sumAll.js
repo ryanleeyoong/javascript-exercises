37bf14af0706e29b84cf2b7e5c52bb3d9c6cd1d8
ryanleeyoong <shapiferoaf@gmail.com>
2023-01-11 22:22:46 +0800

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,26 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+
+    let sum = 0;
+
+    if (num1 < num2 && num1 > 0 && num2 > 0 && typeof(num1) == 'number' && typeof(num2) == 'number') {
+        for (let i = num1; i <= num2; i++) { 
+            sum += i;
+        };
+
+        return sum;
+    }
+    else if (num1 > num2 && num1 > 0 && num2 > 0 && typeof(num1) == 'number' && typeof(num2) == 'number'){
+        for (let i = num2; i <= num1; i++) { 
+            sum += i;
+        };
+
+        return sum;
+    }
+    else {
+        return "ERROR";
+    };
+    
+
 
 };
 
